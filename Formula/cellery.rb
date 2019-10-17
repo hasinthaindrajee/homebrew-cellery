418ee6bc032d2d75658fca96c147fba89b9ccafd
hasinthaindrajee <hasinthaindrajee@gmail.com>
2019-10-17 15:37:57 +0530

    d

--- Formula/cellery.rb
+++ Formula/cellery.rb
@@ -13,7 +13,7 @@ class Cellery < Formula
 
   def install
     bin.install "Library/Cellery/cellery"
-    bin.install "Library/Cellery"
+    bin.install "Library/Cellery/*"
     libexec.install Dir["*"]
   end
 

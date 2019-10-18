804e7de276594564138b34488f9d30088ff8d9a8
hasinthaindrajee <hasinthaindrajee@gmail.com>
2019-10-18 11:06:25 +0530

    d

--- Formula/cellery.rb
+++ Formula/cellery.rb
@@ -15,7 +15,7 @@ class Cellery < Formula
 # bin.install "Library/Cellery/cellery"
     bin.install "cellery"
     libexec.install Dir["*"]
-    system "mkdir", "testing"
+    system "mkdir", "/usr/local/Cellar/hasiiiii"
    # (/usr/local/Cellar/"test").install "cellery"
    # FileUtils.cp("cellery", ".")
   end

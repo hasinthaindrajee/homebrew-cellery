480ab7677d0b19dddad729ae14197fcc7f390c25
hasinthaindrajee <hasinthaindrajee@gmail.com>
2019-10-25 10:04:30 +0530

    cask

--- Casks/ballerina.rb
+++ Casks/ballerina.rb
@@ -2,11 +2,12 @@ cask 'ballerina' do
   version '0.991'
   sha256 :no_check
 
-  url "https://product-dist.ballerina.io/downloads/1.0.1/ballerina-macos-installer-x64-1.0.1.pkg"
+  url "https://product-dist.ballerina.io/downloads/0.991.0/ballerina-macos-installer-x64-0.991.0.pkg"
 
   name ''
   homepage ''
 
   pkg 'ballerina-macos-installer-x64-0.991.0.pkg'
+
   uninstall pkgutil: "org.ballerina.*"
 end
--- Casks/cellery.rb
+++ Casks/cellery.rb
@@ -1,16 +1,22 @@
+class RRequirement < Requirement
+  fatal true
+
+  satisfy(:build_env => false) { which("R") }
+
+end
+
 cask 'cellery' do
-  version '0.5.0'
+  version 'latest'
   sha256 :no_check
 
   url "https://cellery-installer-resources.s3.amazonaws.com/cellery-macos-installer-x64-latest.pkg"
   name 'Cellery'
-  homepage ''
-
+  homepage 'https://wso2-cellery.github.io/'
+  
+  depends_on cask: 'ballerina'
   depends_on formula: 'python'
   depends_on formula: 'torsocks'
-
   pkg 'cellery-macos-installer-x64-latest.pkg'
-
  
   uninstall pkgutil: 'org.cellery.*'
 
